85df0f0ccbbbc99519cb21df59753a85484e1b42
yacut <roman.rogozhnikov@gmail.com>
2017-08-20 13:43:51 +0200

    better kafka-setup scripts and add npm scripts

--- kafka-setup/start.sh
+++ kafka-setup/start.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 rm -rf /tmp/kafka-data
 mkdir /tmp/kafka-data
 mkdir /tmp/kafka-data/data
 mkdir /tmp/kafka-data/logs
 chmod -R 777 /tmp/kafka-data
 
-./generate-certs.sh
+BASEDIR=$(dirname "$0")
 
-docker-compose rm
-docker-compose up -d
+if [[ -z "$(docker-compose --file ${BASEDIR}/docker-compose.yml ps -q)" ]]; then
+	${BASEDIR}/generate-certs.sh
+fi
+
+docker-compose --file ${BASEDIR}/docker-compose.yml rm
+docker-compose --file ${BASEDIR}/docker-compose.yml up -d
