1cb7879e57e1fb5271f3d5054604fe2046036747
Christian Froehlingsdorf <christian.froehlingsdorf@rewe-digital.com>
2017-09-12 15:39:10 +0200

    added streaming mode to nconsumer

--- sasl-ssl-example/consumer.js
+++ sasl-ssl-example/consumer.js
@@ -6,9 +6,16 @@ const consumer = new NConsumer("test", config);
 
 consumer.on("error", error => config.logger.error(error));
 
+/* Flow Mode */
 consumer.connect().then(() => {
   config.logger.info("connected");
   consumer.consume();
 }).catch(error => config.logger.error(error));
 
-consumer.on("message", message => config.logger.log(message.offset, message.value));
+/* Streaming Mode
+consumer.connect(true, {asString: true, asJSON: false}).then(() => {
+  config.logger.info("connected");
+}).catch(error => config.logger.error(error));
+*/
+
+consumer.on("message", message => config.logger.info(message.offset, message.value));
